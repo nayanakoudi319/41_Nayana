f0fbc97f2273e2b69b30f56766df5e4a8f87b6ab
nayana-a-alt <nayanakoudi319@gmail.com>
2024-12-30 05:39:34 +0530

    ads

---
+++ pythonfile.py
@@ -0,0 +1,16 @@
+# Ask the user for input
+num = int(input("Enter a number: "))
+
+# For loop to print the square of each number from 1 to num
+print("\nSquares of numbers from 1 to", num)
+for i in range(1, num + 1):
+    print(f"{i} squared is {i**2}")
+
+# While loop to count down from num to 1
+print("\nCountdown from", num)
+while num > 0:
+    print(num)
+    num -= 1
+
+print("Done!")
+[A[A[A[A[A[A[A[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B
